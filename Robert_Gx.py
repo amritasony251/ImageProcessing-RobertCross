0aea0f193b69a3b46f223cb23f7e4c825030613f
amritasony251 <39574158+amritasony251@users.noreply.github.com>
2020-05-19 04:54:48 +0530

    Add files via upload

---
+++ Robert_Gx.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+img = cv2.imread("img6.bmp")
+height, width, depth = np.shape(img)
+
+# x-kernel     
+Gx = [[1, 0],
+      [0,-1]]
+
+# y-kernel      
+Gy = [[1,0],
+      [0,-1]]
+     
+H = int(len(Gx))
+
+for i in range(0, height-H):
+    for j in range(0, width-H):
+        summ = 0
+        for k in range(0, H):
+            for l in range(0, H):
+                summ = summ + Gy[k][l]*img[i+k][j+l]  
+        if(summ[0]>255):
+           summ = [255,255,255]
+        elif(summ[0]<0):
+           summ = [0,0,0]          
+        img[i][j] = summ  
+            
+
+cv2.imshow('image',img)
+cv2.waitKey(0)            
